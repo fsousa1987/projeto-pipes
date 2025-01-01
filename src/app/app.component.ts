60dc0905f3ed9b3f6af885181be172ca2d8951c9
Francisco Sousa <franciscoeds1987@gmail.com>
2025-01-01 12:08:21 -0300

    Criação base do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'projeto-pipes';
+  operations = [
+    {
+      customerName: 'João',
+      customerStatus: 1,
+      operationDate: '2023-10-31T17:30:00.00Z',
+      operationValue: 500.00,
+      operationRisck: 0.40,
+    },
+    {
+      customerName: 'Maria',
+      customerStatus: 2,
+      operationDate: '2023-10-30T13:15:00.00Z',
+      operationValue: 750.99,
+      operationRisck: 0.0675,
+    },
+    {
+      customerName: 'Pedro',
+      customerStatus: 1,
+      operationDate: '2023-10-29T01:15:00.00Z',
+      operationValue: 3000000.00,
+      operationRisck: 1.0,
+    },
+    {
+      customerName: 'Laura',
+      customerStatus: 2,
+      operationDate: '2023-10-25T03:25:00.00Z',
+      operationValue: 150300.35,
+      operationRisck: 0.10315,
+    },
+  ];
 }
